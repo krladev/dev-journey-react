bb72ddea859d65adc97c4d0d60f5604aa74c515a
Karla Navas <dev@karlanavas.com>
2020-11-20 21:48:39 +1100

    adding npm bootstrap

--- src/DevJourneyApp.js
+++ src/DevJourneyApp.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { AppBody } from './components/AppBody'
 import { AppHeader } from './components/AppHeader'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './css/desktop.css';
 import './css/tablet.css';
 import './css/small-mobile.css';
