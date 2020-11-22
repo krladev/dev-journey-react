bd2f96ccd63d60a3dc84ce84da8c3d147ad2f27c
Karla Navas <dev@karlanavas.com>
2020-11-22 23:40:02 +1100

    data

---
+++ src/data/techStack.js
@@ -0,0 +1,198 @@
+export const techStack = [
+    {
+        "techIcon": "fab fa-html5",
+        "techTitle": "HTML 5",
+        "tags": [
+            "Front-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "HTML5 tags, viewport, etc"
+        },
+        "stars": 5,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-css3",
+        "techTitle": "CSS 3",
+        "tags": [
+            "Front-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Classes, lots of properties, etc. There could be things I still don't know, hence the 4.5 stars :("
+        },
+        "stars": 4.5,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-js-square",
+        "techTitle": "Javascript",
+        "tags": [
+            "Front-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Browser based JS, Classes, ES5, ES6, Async/Await, Modules"
+        },
+        "stars": 3.5,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-react",
+        "techTitle": "ReactJS",
+        "tags": [
+            "Front-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Basic knowledge so far"
+        },
+        "stars": 1,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-vuejs",
+        "techTitle": "VueJS",
+        "tags": [
+            "Front-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-php",
+        "techTitle": "Modern PHP",
+        "tags": [
+            "Back-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Basic Laravel"
+        },
+        "stars": 2,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fas fa-database",
+        "techTitle": "RDBMS",
+        "tags": [
+            "Back-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Basic SQL statements to create schemas and do CRUD operations"
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-aws",
+        "techTitle": "AWS",
+        "tags": [
+            "Devops"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-git-square",
+        "techTitle": "Git",
+        "tags": [
+            "Soft. engineering"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "The basics"
+        },
+        "stars": 2.5,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-linux",
+        "techTitle": "Linux Admin",
+        "tags": [
+            "Devops"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-node",
+        "techTitle": "Node",
+        "tags": [
+            "Back-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-python",
+        "techTitle": "Python",
+        "tags": [
+            "Back-end"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-foursquare",
+        "techTitle": "Flutter",
+        "tags": [
+            "Mobile Dev"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    },
+    {
+        "techIcon": "fab fa-creative-commons-sa",
+        "techTitle": "Ionic",
+        "tags": [
+            "Mobile Dev"
+        ],
+        "infoDescription": {
+            "title": "Things I know",
+            "description": "Nothing yet."
+        },
+        "stars": 0,
+        "lastUpdated": "2020-10-20T22:00:00+10:00",
+        "avatar": "avatar-normal.jpg"
+    }
+];
