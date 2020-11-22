0e575e849bd02bf88e529c84bb8964c132e67764
Karla Navas <dev@karlanavas.com>
2020-11-22 23:54:15 +1100

    state management for search field

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AppHeader = () => {
+    const [keywords, setKeywords] = useState('');
+
+    const handleKeywords = (e) => {
+        setKeywords(e.target.value);
+    };
+
     return (
         <header>
             <div className="title-container">
                 <h1>Karla Navas Dev Journey</h1>
-                <input id="searchField" type="text" className="form-search" autoComplete="off" placeholder="Tech term search"/>
+                <input 
+                    type="text" 
+                    className="form-search" 
+                    autoComplete="off" 
+                    placeholder="Tech term search"
+                    value={keywords}
+                    onChange={ handleKeywords }
+                />
             </div>
             <div className="pic-container">
-                <div><img src="./assets/images/avatar-normal.jpg" /></div>
+                <div><img src="./assets/images/avatar-normal.jpg" alt="Me" /></div>
                 <div id="wittyComment" className="witty">hello!</div>
             </div>
         </header>
